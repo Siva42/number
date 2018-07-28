38a08399644f321d8a3b8ca9290c159bf0cb0f41
Siva42 <41813505+Siva42@users.noreply.github.com>
2018-07-28 15:41:54 +0530

    Create sum100.java

---
+++ sum100.java
@@ -0,0 +1,13 @@
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int sum=0,num=100;
+	for(int i=0;i<=num;i++)
+	{
+		sum=sum+i;
+	}
+	System.out.println("sum:" +sum);
+	}// your code goes here
+	}
