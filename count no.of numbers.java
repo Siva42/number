62ecf7a08448b7b6dcab7240de00f2e3abb4f26a
Siva42 <41813505+Siva42@users.noreply.github.com>
2018-07-28 16:35:41 +0530

    Create count no.of numbers.java

---
+++ count no.of numbers.java
@@ -0,0 +1,21 @@
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int count=0,num;
+		Scanner sc=new Scanner(System.in);
+		num=sc.nextInt();
+		        while(num != 0)
+{
+	num=num/10;
+	count++;
+	
+}
+	
+		System.out.println("number of digits" +count);
+	
+}	// your code goes here
+	
+}
