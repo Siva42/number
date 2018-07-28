231c895a48f962d786c5efe4691f735752f7db6f
Siva42 <41813505+Siva42@users.noreply.github.com>
2018-07-28 12:25:24 +0530

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,22 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+{
+      		char ch = 'e';
+
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{
+System.out.println("vowels");
+}
+else if(ch>='a'&& ch<='z')
+{
+System.out.println("consonants");
+
+}
+else
+{
+System.out.println("invaild");
+}
+
+}
+}
