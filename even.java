dfa3d25ac23cc98505c1dbf013abd9aaf7944555
Siva42 <41813505+Siva42@users.noreply.github.com>
2018-07-28 12:22:06 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,20 @@
+class ideone
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+{
+
+    int num;
+Scanner sc=new Scanner(System.in);
+num=sc.next Int();
+		if(num%2==0)
+{
+System.out.println("even number");
+}
+else
+{
+System.out.println("odd number");
+}
+
+}
+}
