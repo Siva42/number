0e0b95c9d2eb7bc95cb8b5027aa2dcda2d55a114
Siva42 <41813505+Siva42@users.noreply.github.com>
2018-07-28 12:15:17 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,19 @@
+class ideone
+{
+public static void main (String[] args) throws java.lang.Exception
+	{
+
+	int number;
+        Scanner sc=new Scanner(System.in);
+        number =sc.nextInt();
+        if(number>0)
+{
+       System.out.prinln("positive number"):
+}
+      else if(number<0)
+{
+
+System.out.println("negative number");
+}
+}
+}
