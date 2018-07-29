8ec7c7dc76f11ab0abf8af56586477d090b24b29
Siva42 <41813505+Siva42@users.noreply.github.com>
2018-07-29 10:23:03 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -6,11 +6,11 @@ class Prime
 int num,i=2;
 Scanner sc=new Scanner(System.in);
 num=sc.nextInt();
-while(i<2)
+if(i==2)
 {
 	System.out.println("prime number");
 }
-if(num%i==0)
+ else if(num%i==0)
 	{
 		
 		System.out.println(" not a prime number");
