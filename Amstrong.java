c1b48ba9f7d55e93a1ad28b70428faf7f1a17d9a
Siva42 <41813505+Siva42@users.noreply.github.com>
2018-07-29 11:32:39 +0530

    Create Amstrong.java

---
+++ Amstrong.java
@@ -0,0 +1,27 @@
+
+class Amstrong
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int num,c=0,a,temp;
+	Scanner sc=new Scanner(System.in);
+	num=sc.nextInt();
+	temp=num;
+	while(num>0)
+	{// your code goes here
+        
+         a=num%10;
+         num=num/10;
+         c=c+(a*a*a);
+}
+if(temp==c)
+{
+	System.out.println("yes");
+}
+
+else
+{
+	System.out.println("no");
+}
+}
+}
