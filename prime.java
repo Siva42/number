5334fb71307c933437a41ab076b287bf097c6ab2
Siva42 <41813505+Siva42@users.noreply.github.com>
2018-07-29 10:14:22 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,23 @@
+
+class Prime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+int num,i=2;
+Scanner sc=new Scanner(System.in);
+num=sc.nextInt();
+while(i<2)
+{
+	System.out.println("prime number");
+}
+if(num%i==0)
+	{
+		
+		System.out.println(" not a prime number");
+}
+else
+{
+			System.out.println(" prime number");
+}
+}
+}
