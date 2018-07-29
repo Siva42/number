859366e7c56fa619568973d95b0a15060d258773
Siva42 <41813505+Siva42@users.noreply.github.com>
2018-07-29 09:08:28 +0530

    Create pow.java

---
+++ pow.java
@@ -0,0 +1,18 @@
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+
+   
+
+        int base , exponent ;
+        Scanner sc=new Scanner(System.in);
+        base= sc.nextInt();
+        exponent=sc.nextInt();
+        double result = Math.pow(base, exponent);
+
+        System.out.println("Answer = " + result);// your code goes here
+	}
+}
