6b1ad94248aaca944495df48ee701d2a085903c2
Siva42 <41813505+Siva42@users.noreply.github.com>
2018-08-07 11:13:25 +0530

    Update Hour.java

--- Hour.java
+++ Hour.java
@@ -7,10 +7,10 @@ class Hour
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		        Scanner in = new Scanner(System.in);
-        System.out.print("Input seconds: ");
-		int seconds = in.nextInt(); 
-        int p1 = seconds % 60;
-                int p2 = seconds / 60;
+        System.out.print("hh:mm ");
+		int min = in.nextInt(); 
+        int p1 = min % 60;
+                int p2 = min / 60;
 
         int p3 = p2% 60;
                 p2 = p2 / 60;
