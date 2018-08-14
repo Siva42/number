545aff0d7064a8292dc461c7dc4405eae868db0d
Siva42 <41813505+Siva42@users.noreply.github.com>
2018-08-14 11:25:06 +0530

    Create Suma.java

---
+++ Suma.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int a,b,sum=0;
+	Scanner sc=new Scanner(System.in);
+	a=sc.nextInt();
+	b=sc.nextInt();
+	sum=a+b;
+	System.out.print(sum);
+	}
+}
