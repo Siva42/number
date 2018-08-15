6668a4980d55eea8312122330ded3cc3ba7004eb
Siva42 <41813505+Siva42@users.noreply.github.com>
2018-08-15 09:59:41 +0530

    Update num.java

--- num.java
+++ num.java
@@ -1,19 +1,28 @@
-class ideone
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
 {
-public static void main (String[] args) throws java.lang.Exception
+	public static void main (String[] args) throws java.lang.Exception
 	{
-
-	int number;
-        Scanner sc=new Scanner(System.in);
-        number =sc.nextInt();
-        if(number>0)
-{
-       System.out.prinln("positive number"):
-}
-      else if(number<0)
-{
-
-System.out.println("negative number");
-}
-}
-}
+		int number;
+		Scanner sc=new Scanner(System.in);
+		number=sc.nextInt();
+		if(number==0)
+		{
+			System.out.println("zero");
+		
+		}
+		else if(number>0)
+		{
+			System.out.println("positive");
+			
+		}
+		
+		}
+		}// your code goes here
+	
